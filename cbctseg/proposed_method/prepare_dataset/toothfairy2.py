4e1a3ca4222651062c81737e6939ed581a47a036
Fabian Isensee <f.isensee@dkfz.de>
2024-07-02 12:12:55 +0200

    add stuff for toothfairy

--- cbctseg/proposed_method/prepare_dataset/toothfairy2.py
+++ cbctseg/proposed_method/prepare_dataset/toothfairy2.py
@@ -1,1 +1,22 @@
-from 
+from multiprocessing import set_start_method
+from batchgenerators.utilities.file_and_folder_operations import *
+from nnunetv2.paths import nnUNet_raw
+from nnunetv2.utilities.dataset_name_id_conversion import maybe_convert_to_dataset_name
+
+from cbctseg.proposed_method.prepare_dataset.instance_segmentation import convert_sem_dataset_to_instance
+from cbctseg.proposed_method.prepare_dataset.semantic_segmentation import convert_dataset
+
+if __name__ == '__main__':
+    set_start_method('spawn')
+    source_dataset = maybe_convert_to_dataset_name(164)
+    source_dir = join(nnUNet_raw, source_dataset)
+
+    convert_dataset(source_dir, f'Dataset117_ToothFairy2fixed_teeth_spacing02', (0.2, 0.2, 0.2))
+
+    convert_sem_dataset_to_instance(
+        maybe_convert_to_dataset_name(117),
+        'Dataset118_ToothFairy2fixed_teeth_spacing02_brd3px',
+        0.2,
+        3,
+        num_processes=16
+    )
--- cbctseg/proposed_method/toothfairy2.md
+++ cbctseg/proposed_method/toothfairy2.md
@@ -1,13 +1,8 @@
 # Dataset Prep
 1. Convert dataset to nifti with `convert_to_nifti.py`
 2. Remove NA labels with `remove_na_classes.py`
-3. Create teeth only dataset with `teeth_only.py`. This will be our semantic datasets
-4. Create instance segmentation dataset with `prepare_dataset/instance_segmentation.py`:
-   - We need to pick a target spacing here already because the border-core representation is sensitive to that. 
-   When using toothfairy for our paper we keep things consistent, so 0.2 it is even though all tooth fairy data is 0.3. 
-   Changing target spacing here would be test set overfitting. When participating in the challenge we can pick 0.3 because 0.3 makes sense
-   - 
-
-
+3. Create teeth only dataset with `teeth_only.py`. This will be our semantic dataset
+4. Run `prepare_dataset/toothfairy2.py` to create the instance seg dataset. This requires a GPU!
+5. 
 
 # nnU-Net planning and preprocessing
