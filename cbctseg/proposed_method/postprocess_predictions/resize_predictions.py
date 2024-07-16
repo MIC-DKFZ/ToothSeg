e076d3cf7495f3aec0bfecc06784ee53fa1e382a
Fabian Isensee <f.isensee@dkfz.de>
2024-07-16 12:11:08 +0200

    wip

--- cbctseg/proposed_method/postprocess_predictions/resize_predictions.py
+++ cbctseg/proposed_method/postprocess_predictions/resize_predictions.py
@@ -1,4 +1,6 @@
 from multiprocessing import set_start_method
+from multiprocessing.pool import ThreadPool
+from time import sleep
 
 import SimpleITK as sitk
 import numpy as np
@@ -7,34 +9,109 @@ from batchgenerators.utilities.file_and_folder_operations import *
 from nnunetv2.preprocessing.resampling.resample_torch import resample_torch_simple
 
 
-def resample_segmentations_to_ref(ref_folder, pred_folder, target_folder, overwrite=False, num_threads=128):
-    maybe_mkdir_p(target_folder)
+def resample_segmentations_to_ref(ref_folder, pred_folder, output_folder, overwrite=False,
+                                  num_threads_cpu_resampling=128, threads_loading: int = 16,
+                                  threads_saving: int = 8):
+    """
+    This function circumvents I/O problems through threading. Its fast AF boiiiii
+
+    If _loader is too fast this function can run into OOM problems because it needs to store so many intermediate
+    results. If we run into that problem we should just rewrite it to use a queue with fixed length for communication.
+    Couldn't be assed to do this right now.
+    Cheapo solution is to reduce threads_loading.
+    """
+
+    def _loader(casename):
+        print(os.path.basename(casename))
+        seg = sitk.GetArrayFromImage(sitk.ReadImage(join(pred_folder, casename))).astype(np.uint8)
+        target_file = join(ref_folder, casename)
+        if not isfile(target_file):
+            # when the target file is an image it will have the _0000 suffix
+            target_file = target_file[:-7] + '_0000.nii.gz'
+        target_itk_img = sitk.ReadImage(target_file)
+        target_shape = sitk.GetArrayFromImage(target_itk_img).shape
+        target_spacing = target_itk_img.GetSpacing()
+        target_direction = target_itk_img.GetDirection()
+        target_origin = target_itk_img.GetOrigin()
+        return casename, seg, target_shape, target_spacing, target_direction, target_origin
+
+    def _saver(cn, seg_res, target_spacing, target_direction, target_origin):
+        seg_resampled_itk = sitk.GetImageFromArray(seg_res)
+        seg_resampled_itk.SetDirection(target_direction)
+        seg_resampled_itk.SetSpacing(target_spacing)
+        seg_resampled_itk.SetOrigin(target_origin)
+        sitk.WriteImage(seg_resampled_itk, join(output_folder, cn))
+
+    maybe_mkdir_p(output_folder)
     files = nifti_files(pred_folder, join=False)
+
+    tp = ThreadPool(processes=threads_loading)
+    load_res = []
     for f in files:
-        if overwrite or not isfile(join(target_folder, f)):
-            print(os.path.basename(f))
-            seg = sitk.GetArrayFromImage(sitk.ReadImage(join(pred_folder, f))).astype(np.uint8)
-            target_file = join(ref_folder, f)
-            if not isfile(target_file):
-                # when the target file is an image it will have the _0000 suffix
-                target_file = target_file[:-7] + '_0000.nii.gz'
-            target_itk_img = sitk.ReadImage(target_file)
-            target_shape = sitk.GetArrayFromImage(target_itk_img).shape
-            try:
-                seg_resampled = \
-                    resample_torch_simple(torch.from_numpy(seg)[None], target_shape, is_seg=True,
-                                   num_threads=num_threads,
-                                   device=torch.device('cuda:0'))[0].numpy()
-            except:
-                print(f'CPU, file: {f}')
-                seg_resampled = \
-                    resample_torch_simple(torch.from_numpy(seg)[None], target_shape, is_seg=True,
-                                   num_threads=num_threads,
-                                   device=torch.device('cpu'))[0].numpy()
-            torch.cuda.empty_cache()
-            seg_resampled_itk = sitk.GetImageFromArray(seg_resampled)
-            seg_resampled_itk.CopyInformation(target_itk_img)
-            sitk.WriteImage(seg_resampled_itk, join(target_folder, f))
+        if not overwrite and isfile(join(output_folder, f)):
+            continue
+        load_res.append(
+            tp.starmap_async(_loader,
+                             ((f, ),))
+        )
+
+    tps = ThreadPool(processes=threads_saving)
+    save_res = []
+
+    while len(load_res) > 0:
+        for i in range(len(load_res)):
+            if load_res[i].ready():
+                c, s, trg_shp, ts, td, to = load_res.pop(i).get()[0]
+                try:
+                    print(f'GPU resampling for {c}')
+                    seg_resampled = \
+                        resample_torch_simple(torch.from_numpy(s)[None], trg_shp, is_seg=True,
+                                              num_threads=1,
+                                              device=torch.device('cuda:0'))[0].numpy()
+                except:
+                    print(f'GPU failed. Using CPU for: {c}')
+                    seg_resampled = \
+                        resample_torch_simple(torch.from_numpy(s)[None], trg_shp, is_seg=True,
+                                              num_threads=num_threads_cpu_resampling,
+                                              device=torch.device('cpu'))[0].numpy()
+                save_res.append(tps.starmap_async(
+                    _saver, ((c, seg_resampled, ts, td, to),)
+                ))
+                break
+            if i == len(load_res) - 1:
+                sleep(0.1)
+    tp.close()
+    tp.join()
+
+    _ = [i.get() for i in save_res]
+    tps.close()
+    tps.join()
+
+    # for f in files:
+    #     if overwrite or not isfile(join(target_folder, f)):
+    #         print(os.path.basename(f))
+    #         seg = sitk.GetArrayFromImage(sitk.ReadImage(join(pred_folder, f))).astype(np.uint8)
+    #         target_file = join(ref_folder, f)
+    #         if not isfile(target_file):
+    #             # when the target file is an image it will have the _0000 suffix
+    #             target_file = target_file[:-7] + '_0000.nii.gz'
+    #         target_itk_img = sitk.ReadImage(target_file)
+    #         target_shape = sitk.GetArrayFromImage(target_itk_img).shape
+    #         try:
+    #             seg_resampled = \
+    #                 resample_torch_simple(torch.from_numpy(seg)[None], target_shape, is_seg=True,
+    #                                num_threads=num_threads,
+    #                                device=torch.device('cuda:0'))[0].numpy()
+    #         except:
+    #             print(f'CPU, file: {f}')
+    #             seg_resampled = \
+    #                 resample_torch_simple(torch.from_numpy(seg)[None], target_shape, is_seg=True,
+    #                                num_threads=num_threads,
+    #                                device=torch.device('cpu'))[0].numpy()
+    #         torch.cuda.empty_cache()
+    #         seg_resampled_itk = sitk.GetImageFromArray(seg_resampled)
+    #         seg_resampled_itk.CopyInformation(target_itk_img)
+    #         sitk.WriteImage(seg_resampled_itk, join(target_folder, f))
 
 
 if __name__ == '__main__':
@@ -54,14 +131,14 @@ if __name__ == '__main__':
                         help="Reference folder. Must contain files with the same name as the segmentations in -i. "
                              "Segmentations will be reshaped to their counterparts in -ref")
     parser.add_argument('-np', type=int, required=False, default=8,
-                        help='Number of processes used for multiprocessing. Default: 8. Make this a lot higher if you '
-                             'can!')
+                        help='Number of threads used for resampling on CPU fallback (if GPU fails). Default: 8. '
+                             'Make this a lot higher if you can!')
     parser.add_argument('--overwrite_existing', action='store_true',
                         help='By default the script will skip existing results. Set this flag to overwrite (recompute) '
                              'them instead.')
     args = parser.parse_args()
 
-    resample_segmentations_to_ref(args.ref, args.i, args.o, overwrite=args.overwrite_existing, num_threads=args.np)
+    resample_segmentations_to_ref(args.ref, args.i, args.o, overwrite=args.overwrite_existing, num_threads_cpu_resampling=args.np)
 
     # folders_pred = [
     #     # semantic segs
--- cbctseg/proposed_method/toothfairy2.md
+++ cbctseg/proposed_method/toothfairy2.md
@@ -5,60 +5,81 @@
 4. Run `prepare_dataset/toothfairy2.py` to create the instance seg dataset. This requires a GPU and lot of CPUs! Be smart about how you configure this script and where you run it
 
 # nnU-Net planning and preprocessing
-- `nnUNetv2_extract_fingerprint -d 116 118 -np 64`
-- `nnUNetv2_plan_experiment -d 116 118`
+- `nnUNetv2_extract_fingerprint -d 121 123 -np 64`
+- `nnUNetv2_plan_experiment -d 121 123`
 
 # Manual plans editing
-Paste the snippets from [here](readme.md#instance-segmentation-plans) into Dataset118 and 
-from [here](readme.md#semantic-segmentation-plans) into Dataset116 configurations!
+Paste the snippets from [here](readme.md#instance-segmentation-plans) into Dataset123 and 
+from [here](readme.md#semantic-segmentation-plans) into Dataset121 configurations!
 
 # Preprocessing
-- `nnUNetv2_preprocess -d 118 -c 3d_fullres_resample_torch_192_bs8 -np 32`
-- `nnUNetv2_preprocess -d 116 -c 3d_fullres_resample_torch_256_bs8 -np 32`
+- `nnUNetv2_preprocess -d 123 -c 3d_fullres_resample_torch_192_bs8 -np 32`
+- `nnUNetv2_preprocess -d 121 -c 3d_fullres_resample_torch_256_bs8 -np 32`
 
 # Run trainings
 Semantic seg:\
-`nnUNetv2_train 116 3d_fullres_resample_torch_256_bs8 0 -tr nnUNetTrainer_onlyMirror01_DASegOrd0 -num_gpus 8`
+`nnUNetv2_train 121 3d_fullres_resample_torch_256_bs8 0 -tr nnUNetTrainer_onlyMirror01_DASegOrd0 -num_gpus 8`
 
-bsub -gpu num=8:j_exclusive=yes:gmem=33G -q gpu "source ~/load_env_torch221.sh && nnUNetv2_train 116 3d_fullres_resample_torch_256_bs8 0 -tr nnUNetTrainer_onlyMirror01_DASegOrd0 -num_gpus 8"
+bsub -gpu num=8:j_exclusive=yes:gmem=33G -q gpu "source ~/load_env_torch221.sh && nnUNetv2_train 121 3d_fullres_resample_torch_256_bs8 0 -tr nnUNetTrainer_onlyMirror01_DASegOrd0 -num_gpus 8"
 
 Instance seg:\
-`nnUNetv2_train 118 3d_fullres_resample_torch_192_bs8 0 -num_gpus 4`
+`nnUNetv2_train 123 3d_fullres_resample_torch_192_bs8 0 -num_gpus 4`
 
 
-bsub -gpu num=4:j_exclusive=yes:gmem=33G -q gpu "source ~/load_env_torch221.sh && nnUNetv2_train 118 3d_fullres_resample_torch_192_bs8 0 -num_gpus 4"
+bsub -gpu num=4:j_exclusive=yes:gmem=33G -q gpu "source ~/load_env_torch221.sh && nnUNetv2_train 123 3d_fullres_resample_torch_192_bs8 0 -num_gpus 4"
 
 # Training evaluation
-1. Collect the cross-validation results so that they are all in one folder:
-  - `nnUNetv2_accumulate_crossval_results 118 -c 3d_fullres_resample_torch_192_bs8`
-  - `nnUNetv2_accumulate_crossval_results 116 -c 3d_fullres_resample_torch_256_bs8 -tr nnUNetTrainer_onlyMirror01_DASegOrd0`
-2. Convert border-core to instances: 
-    ```bash
-    export nnUNet_raw=/omics/groups/OE0441/E132-Projekte/Projects/2024_MICCAI24_ToothFairy2/nnUNet_raw
-    BASE=${nnUNet_results}/Dataset118_ToothFairy2fixed_teeth_spacing02_brd3px/nnUNetTrainer__nnUNetPlans__3d_fullres_resample_torch_192_bs8
-    REF=${nnUNet_raw}/Dataset115_ToothFairy2fixed/imagesTr
-    python postprocess_predictions/border_core_to_instances.py -i ${BASE}/crossval_results_folds_0_1_2_3_4 -o ${BASE}/crossval_results_folds_0_1_2_3_4_instances -np 64
-    python postprocess_predictions/resize_predictions.py -i ${BASE}/crossval_results_folds_0_1_2_3_4_instances -o ${BASE}/crossval_results_folds_0_1_2_3_4_instances_resized -ref ${REF} -np 64
-   ```
-3. Apply tooth labels
-   ```bash
-   IFOLDER=${nnUNet_results}/Dataset118_ToothFairy2fixed_teeth_spacing02_brd3px/nnUNetTrainer__nnUNetPlans__3d_fullres_resample_torch_192_bs8/crossval_results_folds_0_1_2_3_4_instances_resized
-   SFOLDER=${nnUNet_results}/Dataset116_ToothFairy2fixed_teeth/nnUNetTrainer_onlyMirror01_DASegOrd0__nnUNetPlans__3d_fullres_resample_torch_256_bs8/crossval_results_folds_0_1_2_3_4
-   OFOLDER=${nnUNet_results}/Dataset118_ToothFairy2fixed_teeth_spacing02_brd3px/nnUNetTrainer__nnUNetPlans__3d_fullres_resample_torch_192_bs8/final_predictions_merged
-   python postprocess_predictions/assign_tooth_labels.py -ifolder ${IFOLDER} -sfolder ${SFOLDER} -o ${OFOLDER} -np 64
-   ```
-4. Evaluate\
-   Instances only
-   ```bash
-   python evaluation/evaluate_instances.py -i ${OFOLDER} -ref ${nnUNet_raw}/Dataset116_ToothFairy2fixed_teeth/labelsTr -np 64
-   ```
-   With Tooth label
-   ```bash
-   python evaluation/evaluate_instances_with_tooth_label.py -i ${OFOLDER} -ref ${nnUNet_raw}/Dataset116_ToothFairy2fixed_teeth/labelsTr -np 64
-   ```
-   
-   Just the semseg for comparison with our merged labels. Should show a small but consistent gain across metrics for the merged labels
-   ```bash
-   python evaluation/evaluate_instances.py -i ${SFOLDER} -ref ${nnUNet_raw}/Dataset116_ToothFairy2fixed_teeth/labelsTr -np 64
-   python evaluation/evaluate_instances_with_tooth_label.py -i ${SFOLDER} -ref ${nnUNet_raw}/Dataset116_ToothFairy2fixed_teeth/labelsTr -np 64
-   ```
+```bash
+export nnUNet_raw=/omics/groups/OE0441/E132-Projekte/Projects/2024_MICCAI24_ToothFairy2/nnUNet_raw
+CODE_PATH=/home/isensee/git_repos/radbouduni_2023_cbctteethsegmentation
+SEMSEG_DATASET_NAME='Dataset121_ToothFairy2_Teeth'
+INSTSEG_DATASET_NAME='Dataset123_ToothFairy2fixed_teeth_spacing02_brd3px'
+REF_FOLDER_RESAMPLING=${nnUNet_raw}/Dataset119_ToothFairy2_All/imagesTr
+
+INSTSEG_TRAINER=nnUNetTrainer
+SEMSEG_TRAINER=nnUNetTrainer_onlyMirror01_DASegOrd0
+INSTSEG_CONFIG=3d_fullres_resample_torch_192_bs2
+SEMSEG_CONFIG=3d_fullres_resample_torch_192_bs2
+NNUNET_TRAINING_INSTSEG=${INSTSEG_TRAINER}__nnUNetPlans__${INSTSEG_CONFIG}
+NNUNET_TRAINING_SEMSEG=${SEMSEG_TRAINER}__nnUNetPlans__${SEMSEG_CONFIG}
+
+nnUNet_def_n_proc=64 nnUNetv2_accumulate_crossval_results ${INSTSEG_DATASET_NAME} -c ${INSTSEG_CONFIG} -tr ${INSTSEG_TRAINER}
+nnUNet_def_n_proc=64 nnUNetv2_accumulate_crossval_results ${SEMSEG_DATASET_NAME} -c ${SEMSEG_CONFIG} -tr ${SEMSEG_TRAINER}
+
+# border core to instance
+python ${CODE_PATH}/cbctseg/proposed_method/postprocess_predictions/border_core_to_instances.py \
+-i ${nnUNet_results}/${INSTSEG_DATASET_NAME}/${NNUNET_TRAINING_INSTSEG}/crossval_results_folds_0_1_2_3_4 \
+-o ${nnUNet_results}/${INSTSEG_DATASET_NAME}/${NNUNET_TRAINING_INSTSEG}/crossval_results_folds_0_1_2_3_4_instances \
+-np 64
+# resize border core to ref
+python ${CODE_PATH}/cbctseg/proposed_method/postprocess_predictions/resize_predictions.py \
+-i ${nnUNet_results}/${INSTSEG_DATASET_NAME}/${NNUNET_TRAINING_INSTSEG}/crossval_results_folds_0_1_2_3_4_instances \
+-o ${nnUNet_results}/${INSTSEG_DATASET_NAME}/${NNUNET_TRAINING_INSTSEG}/crossval_results_folds_0_1_2_3_4_instances_resized \
+-ref ${REF_FOLDER_RESAMPLING} -np 64
+
+# apply tooth labels
+python ${CODE_PATH}/cbctseg/proposed_method/postprocess_predictions/assign_tooth_labels.py \
+-ifolder ${nnUNet_results}/${INSTSEG_DATASET_NAME}/${NNUNET_TRAINING_INSTSEG}/crossval_results_folds_0_1_2_3_4_instances_resized \
+-sfolder ${nnUNet_results}/${SEMSEG_DATASET_NAME}/${NNUNET_TRAINING_SEMSEG}/crossval_results_folds_0_1_2_3_4 \
+-o ${nnUNet_results}/${INSTSEG_DATASET_NAME}/${NNUNET_TRAINING_INSTSEG}/final_predictions_merged \
+-np 64
+
+# evaluate
+# full eval with tooth labels
+python ${CODE_PATH}/cbctseg/evaluation/evaluate_instances_with_tooth_label.py \
+-i ${nnUNet_results}/${INSTSEG_DATASET_NAME}/${NNUNET_TRAINING_INSTSEG}/final_predictions_merged \
+-ref ${nnUNet_raw}/${SEMSEG_DATASET_NAME}/labelsTr -np 64
+# instances only
+python ${CODE_PATH}/cbctseg/evaluation/evaluate_instances.py \
+-i ${nnUNet_results}/${INSTSEG_DATASET_NAME}/${NNUNET_TRAINING_INSTSEG}/final_predictions_merged \
+-ref ${nnUNet_raw}/${SEMSEG_DATASET_NAME}/labelsTr -np 64
+
+# just the semseg for comparison
+python ${CODE_PATH}/cbctseg/evaluation/evaluate_instances_with_tooth_label.py \
+-i ${nnUNet_results}/${SEMSEG_DATASET_NAME}/${NNUNET_TRAINING_SEMSEG}/crossval_results_folds_0_1_2_3_4 \
+-ref ${nnUNet_raw}/${SEMSEG_DATASET_NAME}/labelsTr -np 64
+# instances only
+python ${CODE_PATH}/cbctseg/evaluation/evaluate_instances.py \
+-i ${nnUNet_results}/${SEMSEG_DATASET_NAME}/${NNUNET_TRAINING_SEMSEG}/crossval_results_folds_0_1_2_3_4 \
+-ref ${nnUNet_raw}/${SEMSEG_DATASET_NAME}/labelsTr -np 64
+```
